29e46a2277fb25f0ac0e5a4c8bb4e150b3dce75e
MarioBland <gabedotgutierrez@gmail.com>
2020-01-23 11:19:15 -0500

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,4 +1,50 @@
+function createStore(reducer) {
+  let state;
+ 
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+ 
+  function getState() {
+    return state;
+  };
+ 
+  return {
+    dispatch,
+    getState
+  };
+};
+ 
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
+ 
+    default:
+      return state;
+  }
+}
+ 
+ 
+function render() {
+  let container = document.getElementById('container');
+  container.textContent = store.getState().count;
+};
+ 
+let store = createStore(reducer) // createStore takes the reducer reducer as an argument
+store.dispatch({ type: '@@INIT' });
+let button = document.getElementById('button');
+ 
+button.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREASE_COUNT' });
+});
+
+
+/* ---ORIGINAL CODE
+
 let state;
+
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -9,6 +55,7 @@ function reducer(state = { count: 0 }, action) {
       return state;
   }
 };
+
 
 function dispatch(action){
   state = reducer(state, action);
@@ -26,3 +73,4 @@ let button = document.getElementById('button');
 button.addEventListener('click', function() {
     dispatch({ type: 'INCREASE_COUNT' });
 })
+*/
